1bb03bc3214a6853f0146c0c3f7f6bd42fa26eba
tosin4u13 <tosin4u13@yahoo.com>
2025-03-11 19:14:38 +0000

    Update course model

--- app/models/course.js
+++ app/models/course.js
@@ -11,8 +11,9 @@ class Course {
         const query = "SELECT * FROM Courses WHERE CourseID = ?";
         return new Promise((resolve, reject) => {
             db.query(query, [this.id], (err, results) => {
-                if (err) reject(err);
-                if (results.length > 0) {
+                if (err) {
+                    reject(err);
+                } else if (results.length > 0) {
                     this.name = results[0].CourseName;
                     resolve(results[0]);
                 } else {
