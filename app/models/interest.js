0ff931dbfa6c5fc9fdd99e18c541521a769d23be
tosin4u13 <tosin4u13@yahoo.com>
2025-03-11 12:43:52 +0000

    Add interest model

---
+++ app/models/interest.js
@@ -0,0 +1,37 @@
+const db = require("../services/db"); // Import the database connection
+
+class Interest {
+    constructor(id) {
+        this.id = id;
+        this.name = null;
+    }
+
+    // Fetch interest details by InterestID
+    async getInterestDetails() {
+        const query = "SELECT * FROM Interests WHERE InterestID = ?";
+        return new Promise((resolve, reject) => {
+            db.query(query, [this.id], (err, results) => {
+                if (err) reject(err);
+                if (results.length > 0) {
+                    this.name = results[0].InterestName;
+                    resolve(results[0]);
+                } else {
+                    resolve(null);
+                }
+            });
+        });
+    }
+
+    // Fetch all interests
+    static async getAllInterests() {
+        const query = "SELECT * FROM Interests";
+        return new Promise((resolve, reject) => {
+            db.query(query, (err, results) => {
+                if (err) reject(err);
+                resolve(results);
+            });
+        });
+    }
+}
+
+module.exports = { Interest };
