99d6ee0618d8db671d8a38bcb1a0675c78114fd0
Michaela <radostom@roehampton.ac.uk>
2025-03-29 12:37:49 +0000

    I temp disabled login authentication, so we can design app

--- app/services/authMiddleware.js
+++ app/services/authMiddleware.js
@@ -1,4 +1,5 @@
 function ensureAuthenticated(req, res, next) {
+  /* TEMP DISABLED FOR DESIGN
     if (req.session.user) {
       // If the User is authenticated, proceed to the next middleware or route handler
       return next();
@@ -6,6 +7,8 @@ function ensureAuthenticated(req, res, next) {
   
     // If the User is not authenticated, redirect to the login page
     res.redirect("/login");
+    */
+   return next();
   }
   
   module.exports = ensureAuthenticated;
